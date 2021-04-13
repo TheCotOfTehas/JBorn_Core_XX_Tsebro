e8b5fb01575be25566a2c1517a9abc5d0761333e
Evgeny <TsebroEvgeny@yandex.ru>
2021-04-13 09:45:29 +0300

    Просто мастер по дефолту без реализации сорт

--- src/s01/Task01.java
+++ src/s01/Task01.java
@@ -4,6 +4,21 @@ public class Task01
 {
     public  static  void  main(String[] args)
     {
+        double ookkk[]={4,5,6,7,3,1,8,55};
+        for (int i=0; i<ookkk.length;i++){
+            System.out.print(ookkk[i]+" ");
+        }
+        System.out.println();
+        double ookkkSort[];
+        ookkkSort=Sort(ookkk);
+        for (int i=0; i<ookkkSort.length;i++){
+            System.out.print(ookkkSort[i]+" ");
+        }
+        System.out.println();
         System.out.println("Hello Natasha");
+    }
+    public static   double[] Sort(double[] number)
+    {
+        return number;
     }
 }
