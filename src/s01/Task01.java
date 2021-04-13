873fba4f005b3bc3d3238a50519229bcc95ea390
Evgeny <TsebroEvgeny@yandex.ru>
2021-04-13 09:24:02 +0300

    Пупырчатая сортировка

--- src/s01/Task01.java
+++ src/s01/Task01.java
@@ -4,6 +4,31 @@ public class Task01
 {
     public  static  void  main(String[] args)
     {
+        double ookkk[]={4,5,6,7,3,1,8,55};
+        for (int i=0; i<ookkk.length;i++){
+            System.out.print(ookkk[i]+" ");
+        }
+        System.out.println();
+        double ookkkSort[];
+        ookkkSort=Sort(ookkk);
+        for (int i=0; i<ookkkSort.length;i++){
+            System.out.print(ookkkSort[i]+" ");
+        }
+        System.out.println();
         System.out.println("Hello Natasha");
+    }
+    public static   double[] Sort(double[] number)
+    {
+        double num;
+        for (int y=0;y<number.length-1;y++){
+            for (int i = 0; i < number.length - 1; i++){
+                if (number[i] > number[i + 1]) {
+                    num = number[i + 1];
+                    number[i + 1] = number[i];
+                    number[i] = num;
+                }
+            }
+        }
+        return number;
     }
 }
